539bfbd2c95b5a54ee3cc585d36688d5cef84373
jeramyadams37-dev <jeramyadams37@gmail.com>
2026-09-06 02:00:42 -0500

    Register Obd2Plugin in MainActivity

--- android/app/src/main/java/com/atothei/autohustle/MainActivity.java
+++ android/app/src/main/java/com/atothei/autohustle/MainActivity.java
@@ -1,5 +1,13 @@
 package com.atothei.autohustle;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.atothei.autohustle.obd2.Obd2Plugin;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(Obd2Plugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
